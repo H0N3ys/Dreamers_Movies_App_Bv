05f4cb62114fa361866b6d17426dffdda24204f6
AdrianCSS <202300185@upqroo.edu.mx>
2026-05-29 17:18:09 -0500

    Refactor de la pantalla login usando componentes (#3)
    Co-authored-by: AdrianPato <rodriguezgomezadrian98@gmail.com>

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,17 +1,22 @@
-
+import 'package:dreamers_movies_app_bv/presentation/screens/movies/home_screen.dart';
 import 'package:go_router/go_router.dart';
-import 'package:dreamers_movies_app_bv/presentation/screens/screen.dart';
-
+import 'package:dreamers_movies_app_bv/presentation/auth/login_screen.dart';
 final appRouter = GoRouter(
-  initialLocation: "/",
-
+  initialLocation: "/login", 
+  
   routes: [
+    // Ruta del Login
     GoRoute(
-      path: ".",
+      path: "/login",
+      name: LoginScreen.name,
+      builder: (context, state) => const LoginScreen(),
+    ),
 
+    // Ruta del Home 
+    GoRoute(
+      path: "/",
       name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),)
-      
-      
+      builder: (context, state) => const HomeScreen(),
+    )
   ]
 );
