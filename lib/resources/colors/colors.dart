653902c09d1d06837897b79e7a1ab8280dc63be7
Pablo-Segovia15 <pablitoclavito2105@gmail.com>
2026-06-14 17:54:05 -0500

    Cambios en colores y .env

--- lib/resources/colors/colors.dart
+++ lib/resources/colors/colors.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color primaryColor = Color(0xFFFFFFFF);
   static const Color secondaryColor = Color(0xFF152A72);
-  static const Color accentColor = Color(0xFF020202);
-}
+  static const Color accentColor = Color(0xFF242424);
+} 
